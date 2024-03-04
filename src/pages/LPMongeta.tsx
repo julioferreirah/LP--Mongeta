6e8f1786876f32381b8b919c310701783b526623
Júlio Ferreira <julioferreira@usp.br>
2024-03-04 17:59:16 -0300

    Initial commit

---
+++ src/pages/LPMongeta.tsx
@@ -0,0 +1,219 @@
+import { FunctionComponent } from "react";
+import styles from "./LPMongeta.module.css";
+
+const LPMongeta: FunctionComponent = () => {
+  return (
+    <div className={styles.lpMongeta}>
+      <img
+        className={styles.fundoTelemarketingIcon}
+        alt=""
+        src="/fundotelemarketing@2x.png"
+      />
+      <div className={styles.wrapperBlurHeadline}>
+        <img className={styles.blurHeadlineIcon} loading="lazy" alt="" />
+      </div>
+      <div className={styles.instagramLink}>
+        <img
+          className={styles.whatsappEmailLinksGroup}
+          loading="lazy"
+          alt=""
+          src="/2-6@2x.png"
+        />
+        <div className={styles.instagram}>Instagram</div>
+      </div>
+      <div className={styles.instagramLink1}>
+        <div className={styles.centrazaoDeCanaisContainer}>
+          <p className={styles.centrazaoDeCanais}>Centrazação de canais</p>
+          <p className={styles.colaboraoDeTimes}>Colaboração de times</p>
+          <p className={styles.automaesDeRespostas}>Automações de respostas</p>
+          <p className={styles.botsDeAtedimento}>Bots de atedimento</p>
+          <p className={styles.notasEClassificaes}>
+            Notas e classificações de contatos
+          </p>
+          <p className={styles.identificaoDeAtendente}>
+            Identificação de atendente
+          </p>
+          <p className={styles.reportesEComandos}>Reportes e comandos</p>
+        </div>
+      </div>
+      <div className={styles.whatsLink}>
+        <img className={styles.icon} loading="lazy" alt="" src="/4-1@2x.png" />
+        <div className={styles.whatsapp}>Whatsapp</div>
+      </div>
+      <div className={styles.emailLink}>
+        <img className={styles.icon1} loading="lazy" alt="" src="/3-1@2x.png" />
+        <div className={styles.email}>Email</div>
+      </div>
+      <div className={styles.tiktokLink}>
+        <img className={styles.icon2} loading="lazy" alt="" src="/5-1@2x.png" />
+        <div className={styles.tiktok}>Tiktok</div>
+      </div>
+      <div className={styles.copyrigthSteffer}>
+        Copyrigth @Steffer - Todos os direitos reservados
+      </div>
+      <header className={styles.cabealho} />
+      <img
+        className={styles.mongetaLogoCabIcon}
+        loading="lazy"
+        alt=""
+        src="/mongetalogocab@2x.png"
+      />
+      <button className={styles.boto}>
+        <div className={styles.saibaMais}>Saiba mais</div>
+      </button>
+      <button className={styles.boto1}>
+        <div className={styles.saibaMais1}>Saiba mais</div>
+      </button>
+      <img
+        className={styles.mongetaLogoFinalIcon}
+        loading="lazy"
+        alt=""
+        src="/mongetalogofinal@2x.png"
+      />
+      <div className={styles.divisriaPrimeiraDobra}>
+        <img
+          className={styles.polygonShapeIcon}
+          loading="lazy"
+          alt=""
+          src="/polygon-3.svg"
+        />
+        <div className={styles.horizontalLine} />
+      </div>
+      <section className={styles.telaMongeta}>
+        <img
+          className={styles.telachathoot1Icon}
+          alt=""
+          src="/telachathoot-1@2x.png"
+        />
+        <img
+          className={styles.iconeStefferCircular1}
+          loading="lazy"
+          alt=""
+          src="/iconesteffercircular-1@2x.png"
+        />
+        <img
+          className={styles.iconeStefferCircular2}
+          alt=""
+          src="/iconesteffercircular-2@2x.png"
+        />
+      </section>
+      <img
+        className={styles.blurBrancoLateral}
+        alt=""
+        src="/blur-branco-lateral.svg"
+      />
+      <div className={styles.wrapperBlurAzulLateral}>
+        <img
+          className={styles.blurAzulLateral}
+          loading="lazy"
+          alt=""
+          src="/blur-azul-lateral.svg"
+        />
+      </div>
+      <div className={styles.wrapperBlurAzulCentral}>
+        <img
+          className={styles.blurAzulCentral}
+          loading="lazy"
+          alt=""
+          src="/blur-azul-central.svg"
+        />
+      </div>
+      <div className={styles.wrapperBlurWhats}>
+        <img className={styles.blurWhats} alt="" src="/blur-whats.svg" />
+      </div>
+      <button className={styles.boto2}>
+        <div className={styles.saibaMais2}>Fale com um especialista</div>
+      </button>
+      <div className={styles.gerencimentoDeConversasContainer}>
+        <span>{`Gerencimento de `}</span>
+        <b>conversas</b>
+        <span>{` e clientes de forma fácil `}</span>
+      </div>
+      <h2 className={styles.centralizeOSeuContainer}>
+        <span className={styles.centralizeOSeu}>{`Centralize o seu `}</span>
+        <b className={styles.atendimento}>atendimento</b>
+        <span>{` ao cliente em `}</span>
+        <span>
+          <span className={styles.umSLuga}>um só luga</span>r
+        </span>
+      </h2>
+      <section className={styles.aumenteSuaProdutividadeContainer}>
+        <span className={styles.aumenteSuaProdutividade}>
+          <span className={styles.aumenteSuaProdutividade1}>
+            Aumente sua produtividade
+          </span>
+        </span>
+        <span>
+          <span className={styles.com}>{` com `}</span>
+          <b className={styles.automaes}>automações</b>
+          <span>{` e colabore com outras pessoas `}</span>
+        </span>
+        <span>
+          <span>
+            <span className={styles.semTrocarDe}>sem trocar de app</span>
+            {` `}
+          </span>
+        </span>
+      </section>
+      <section className={styles.vriosNmerosAparelhosContainer}>
+        {`Vários `}
+        <span className={styles.nmerosAparelhosE}>
+          números, aparelhos e atendentes contectados
+        </span>{" "}
+        com histórico permanente de conversas
+      </section>
+      <img
+        className={styles.mockeupIcon}
+        loading="lazy"
+        alt=""
+        src="/mockeup@2x.png"
+      />
+      <img className={styles.divisorIcon} alt="" src="/divisor.svg" />
+      <img className={styles.whatsLogo1Icon} alt="" src="/whatslogo-1@2x.png" />
+      <img
+        className={styles.centralized11}
+        loading="lazy"
+        alt=""
+        src="/centralized-1-1@2x.png"
+      />
+      <img
+        className={styles.teamWork11}
+        loading="lazy"
+        alt=""
+        src="/teamwork-1-1@2x.png"
+      />
+      <img
+        className={styles.automation1Icon}
+        loading="lazy"
+        alt=""
+        src="/automation-1@2x.png"
+      />
+      <img
+        className={styles.bot1Icon}
+        loading="lazy"
+        alt=""
+        src="/bot-1@2x.png"
+      />
+      <img
+        className={styles.writing1Icon}
+        loading="lazy"
+        alt=""
+        src="/writing-1@2x.png"
+      />
+      <img
+        className={styles.verified1Icon}
+        loading="lazy"
+        alt=""
+        src="/verified-1@2x.png"
+      />
+      <img
+        className={styles.report1Icon}
+        loading="lazy"
+        alt=""
+        src="/report-1@2x.png"
+      />
+    </div>
+  );
+};
+
+export default LPMongeta;
